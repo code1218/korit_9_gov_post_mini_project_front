3543067a2aa0946067f37532e38a90525dc4c1e9
code1218 <skjil1218@gmail.com>
2025-12-17 17:43:53 +0900

    사이드바 끝

--- src/components/common/styles.js
+++ src/components/common/styles.js
@@ -61,9 +61,15 @@ export const sideBarContainer = css`
     background-color: #ffffff;
 
     & > h1 {
+        margin: 20px;
         font-size: 20px;
         text-align: center;
+        text-shadow: 0 0 20px #00000099;
         cursor: default;
+        transition: all 0.3s ease-in-out;
+        &:hover {
+            transform: scale(105%);
+        }
     }
 
     & > ul {
@@ -94,7 +100,7 @@ export const sideBarContainer = css`
     }
 `;
 
-export const menuListItem = (isSeleted) => css`
+export const menuListItem = (isSelected) => css`
     display: flex;
     align-items: center;
     box-sizing: border-box;
@@ -102,13 +108,29 @@ export const menuListItem = (isSeleted) => css`
     width: 100%;
     height: 35px;
     font-weight: 500;
+    transition: all 0.1s ease-in-out;
+    box-shadow: 0 0 ${isSelected ? "10px 10px" : "0 0"} #0000000f inset;
+    &:hover {
+        text-shadow: 0 0 15px #00000066;
+    }
 
     & > div {
         display: flex;
         justify-content: center;
         align-items: center;
+        margin-right: 5px;
         width: 30px;
         height: 100%;
         font-size: 20px;
     }
+`;
+
+export const profileImg = (url) => css`
+    border-radius: 50%;
+    width: 25px;
+    height: 25px;
+    overflow: hidden;
+    background-image: url(${url});
+    background-position: center;
+    background-size: cover;
 `;
