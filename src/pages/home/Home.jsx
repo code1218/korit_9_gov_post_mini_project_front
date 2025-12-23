e92afaa5bf9d19bcdbcf76487523d773db92e641
code1218 <skjil1218@gmail.com>
2025-12-23 17:54:45 +0900

    댓글 만들 준비

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,21 +5,40 @@ import { useGetFeeds } from "../../queries/postQueries";
 import * as s from "./styles";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useEffect, useRef, useState } from "react";
+import { FadeLoader } from "react-spinners";
+import { IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
+import { IoChatbubbleOutline } from "react-icons/io5";
 
 function Home() {
-    const { isLoading, data } = useGetFeeds();
-    console.log("isLoading:", isLoading);
-    console.log("data:", data);
+    const [ commentOpen, setCommentOpen ] = useState(false);
+    const { isLoading, isFetching, data, hasNextPage, fetchNextPage } = useGetFeeds();
+    const loadMoreRef = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            const [obs] = entries;
+            if (obs.isIntersecting && hasNextPage) {
+                fetchNextPage();
+            }
+        }, {threshold: 1});
+
+        observer.observe(loadMoreRef.current);
+    }, [hasNextPage]);
+
+    const handleCommentOnClick = () => {
+        setCommentOpen(!commentOpen);
+    }
 
     return <div css={s.layout}>
-        <div css={s.feedContainer}>
+        <div css={s.feedContainer(commentOpen)}>
             {
                 (isLoading && <Loading />) || data.pages.map(feeds => feeds.data.contents.map(feed => (
                     <div key={feed.feedId} css={s.feedItemContainer}>
                         <header>
-                            <div css={s.profileImage(feed.user.imgUrl)}></div>
+                            <div css={s.profileImage(feed.user?.imgUrl)}></div>
                             <div css={s.userInfo}>
-                                <div>{feed.user.nickname}</div>
+                                <div>{feed.user?.nickname}</div>
                                 <div>{feed.createdAt}</div>
                             </div>
                         </header>
@@ -46,11 +65,20 @@ function Home() {
                             </div>
                         </main>
                         <footer>
-
+                            <div>{false ? <IoMdHeart /> : <IoMdHeartEmpty />}</div>
+                            <div onClick={handleCommentOnClick}><IoChatbubbleOutline /></div>
                         </footer>
                     </div>
                 )))
             }
+            <div ref={loadMoreRef} style={{padding: "10px 0"}}>
+                {
+                    isFetching && !isLoading && <FadeLoader />
+                }
+            </div>
+        </div>
+        <div css={s.commentContainer(commentOpen)}>
+
         </div>
         <div css={s.followInfoContainer}>
 
